4057a53fae944256e565784276a224e2a06102f7
Lazaro21 <olsf21@gmail.com>
2023-02-26 15:06:21 -0300

    FindByDepartment Implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -8,14 +8,24 @@ import db.DB;
 
 import java.sql.Connection;
 import java.util.Date;
+import java.util.List;
 
 public class Program {
 
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao(DB.getConnection());
 
-        System.out.println("===== TEST 1: seller findBy id ======");
+        System.out.println("===== TEST 1: seller findById ======");
         Seller seller =sellerDao.findById(3);
         System.out.println(seller);
+
+        System.out.println("===== TEST 2: seller findByDepartment ======");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        list.stream().forEach(System.out::println);
+//
+//        for(Seller obj : list) {
+//            System.out.println(obj);
+//        }
     }
 }
