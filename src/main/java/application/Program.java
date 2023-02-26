5b2027e7266d533ce7451a1fcabbefcbf9a23fcf
Lazaro21 <olsf21@gmail.com>
2023-02-26 14:15:21 -0300

    findById Implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -4,18 +4,17 @@ import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
+import db.DB;
 
+import java.sql.Connection;
 import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
-        Department obj= new Department(1, "Books");
+        SellerDao sellerDao = DaoFactory.createSellerDao(DB.getConnection());
 
-        Seller seller = new Seller(21, "Bob", "bob@gmail", new Date(), 3000.0, obj);
-
-        SellerDao sellerDao = DaoFactory.createSellerDao();
-
+        Seller seller =sellerDao.findById(3);
         System.out.println(seller);
     }
 }
